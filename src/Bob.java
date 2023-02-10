5e091879ece4d65a1200813e6becfb2d37eae80c
Jeffrey Neal <jeffneal11@gmail.com>
2023-02-10 16:28:52 -0600

    fixed bobber

--- src/Bob.java
+++ src/Bob.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean chatting = true;
         System.out.println("bob");
-        String userInp = sc.nextLine();
-        if(userInp.endsWith("?")){
-            System.out.println("sure");
-        }else if(userInp.endsWith("!")){
-            System.out.println("!!!");
-        } else if (userInp.equals("")) {
-            System.out.println("ok");
+        while (chatting){
+            String userInp = sc.nextLine();
+            if(userInp.endsWith("?")){
+                System.out.println("sure");
+            }else if(userInp.endsWith("!")){
+                System.out.println("!!!");
+            } else if (userInp.equals("")) {
+                System.out.println("ok");
+            } else if(userInp.equals("bye")) {
+                System.out.println("cya");
 
-        } else {
-            System.out.println("whatever");
+                chatting = false;
+            }else{
+                System.out.println("whatever");
+            }
         }
+
     }
 }
